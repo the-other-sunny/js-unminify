1d0f1adfc922b11c855eeb4377e0f3730229c437
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-02-24 22:36:00 +0100

    unnecessary security check

--- src/preProcessing/index.js
+++ src/preProcessing/index.js
@@ -1,60 +1,69 @@
 const t = require('@babel/types');
 const assert = require('node:assert/strict');
 
+/*
+* TODO: Check if this security can be removed.
+* For instance, it seems that a `VariableDeclaration` in a `ForStatement`'s `init` is fortunately not recognized as a `Statement` during the tree traversal.
+* Although the `babel-types` documentation states that a `VariableDeclaration` is a `Statement`.
+* Exploding a `node` in places where only a single `Statement` is expected would be problematic.
+* 
+* NOTE: this check seems to be unnecessary as the tree traverser won't consider a `ForStatement`'s init `VariableDeclaration` as a `Statement` for instance.
+* `Statements` are reliably detected using `path.isStatement()`, whereas `t.isStatement(node)` fails in some situations, as `node` type doesn't provide enough information to determine, for instance, if a `VariableDeclaration` is a `Statement` or not for instance.
+* 
+* Cf. https://github.com/babel/babel/blob/c38bf12f010520ea7abe8a286f62922b2d1e1f1b/packages/babel-traverse/src/path/lib/virtual-types-validator.ts#L118
+*/
+function safetyCheck(path) {
+    switch (path.parent.type) {
+        case 'DoWhileStatement': // unnecessary
+        case 'ForInStatement':
+        case 'ForOfStatement':
+        case 'ForStatement':
+        case 'LabeledStatement': // unnecessary
+        case 'WhileStatement': // unnecessary
+        case 'WithStatement': // unnecessary
+            assert(path.key === 'body')
+            break;
+        case 'IfStatement':
+            assert(path.key === 'consequent' || path.key === 'alternate')
+            break;
+    }
+}
+
 /**
- * Turn `Statement`s into `BlockStatement`s whenever possible so the `Statement` could be exploded if necessary.
+ * Turn `Statement`s into `BlockStatement`s whenever possible so the `Statement` can be exploded if necessary.
  * @param {Path} path 
  * @returns 
  */
 function Statement(path) {
-    function safetyCheck(path) {
-        /*
-        * TODO: Check if this security could be removed.
-        * For instance, it seems that a `VariableDeclaration` in a `ForStatement`'s `init` is not recognized as a `Statement`.
-        * Although the documentation states that a `VariableDeclaration` is a `Statement`.
-        * Misapplying this transformation on that kind of Node would be problematic...
-        * For now, it doesn't occure, but it's not certain that it is the expected behavior.
-        */
-        const parentNodeType = path.parent.type;
-        const key = path.key;
-        const check = {
-            'DoWhileStatement': () => assert(key === 'body'),
-            'ForInStatement':   () => assert(key === 'body'),
-            'ForOfStatement':   () => assert(key === 'body'),
-            'ForStatement':     () => assert(key === 'body'),
-            'IfStatement':      () => assert(key === 'consequent' || key === 'alternate'),
-            'LabeledStatement': () => assert(key === 'body'),
-            'WhileStatement':   () => assert(key === 'body'),
-            'WithStatement':    () => assert(key === 'body')
-        }[parentNodeType];
-        check();
-    }
-
     if (path.isBlockStatement()) {
         return;
     }
-
-    const parentNodeType = path.parent.type;
-    let canBeAStatementBlock = [
-        // Parent nodes susceptible to have a child `StatementBlock`.
-        'DoWhileStatement',
-        'ForInStatement',
-        'ForOfStatement',
-        'ForStatement',
-        'IfStatement',
-        'LabeledStatement',
-        'WhileStatement',
-        'WithStatement'
-    ].includes(parentNodeType);
-    canBeAStatementBlock &&= !(parentNodeType === 'IfStatement' && path.type === 'IfStatement');
-    if (!canBeAStatementBlock) {
-        return;
+    
+    let turnableIntoABlock;
+    switch (path.parent.type) {
+        case 'DoWhileStatement':
+        case 'ForInStatement':
+        case 'ForOfStatement':
+        case 'ForStatement':
+        case 'LabeledStatement':
+        case 'WhileStatement':
+        case 'WithStatement':
+            turnableIntoABlock = true;
+            break;
+        case 'IfStatement':
+            // if parent node is an `IfStatement` and the current node is it's alternate child, it should not be turned into a block if it's an `IfStatement` as it would unnecessarily lengthen the code.
+            turnableIntoABlock = path.type !== 'IfStatement';
+            break;
+        default:
+            turnableIntoABlock = false;
     }
-
-    safetyCheck(path);
-    path.replaceWith(
-        t.blockStatement([path.node])
-    );
+    
+    if (turnableIntoABlock) {
+        // safetyCheck(path); // unnecessary safety check
+        path.replaceWith(
+            t.blockStatement([path.node])
+        );
+    }
 }
 
 module.exports.Statement = Statement;

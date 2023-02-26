159b1b9ada52c6c58e03d6855dfc14eb325a3283
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-02-26 16:01:12 +0100

    feat: logicalANDExpression

--- src/processNode/ExpressionStatement/logicalANDExpression.js
+++ src/processNode/ExpressionStatement/logicalANDExpression.js
@@ -1,7 +1,14 @@
 const t = require('@babel/types');
 
 function logicalANDExpression(path) {
-
+    const logicalExpression = path.node.expression;
+    const { left, right } = logicalExpression;
+    const newNode = t.ifStatement(
+        left,
+        t.blockStatement([t.expressionStatement(right)])
+    );
+    
+    path.replaceWith(newNode);
 }
 
 module.exports = logicalANDExpression;

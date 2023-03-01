41d0309cda907ac2448a1b4d5c418145008caeb5
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-03-01 21:36:44 +0100

    feat: RetunStatement

---
+++ src/processNode/ReturnStatement.js
@@ -0,0 +1,62 @@
+const t = require('@babel/types');
+const isExpandable = require('../../utils').isExpandable;
+
+function returnSequence(path) {
+    if (!isExpandable(path)) {
+        return; // TODO: perhaps, we should throw or warn about that ?
+    }
+
+    const expressions = [...path.node.argument.expressions];
+    const lastExpr = expressions.pop();
+    t.replaceWithMultiple([
+        ...expressions.map(expr => t.expressionStatement(expr)),
+        t.returnStatement(lastExpr)
+    ]);
+}
+
+function returnConditional(path) {
+    if (!isExpandable(path)) {
+        return; // TODO: perhaps, we should throw or warn about that ?
+    }
+
+    const { test, consequent, alternate } = path.node.argument;
+    const ifNode = t.ifStatement(
+        test,
+        t.blockStatement([
+            t.expressionStatement(consequent)
+        ])
+    );
+    const returnNode = t.returnStatement(alternate);
+    path.replaceWithMultiple([ifNode, returnNode]);
+}
+
+function returnVoid(path) {
+    if (!isExpandable(path)) {
+        return; // TODO: perhaps, we should throw or warn about that ?
+    }
+
+    const expression = path.node.argument.argument;
+    path.replaceWithMultiple([
+        t.expressionStatement(expression),
+        t.returnStatement()
+    ]);
+}
+
+function returnUndefined(path) {
+    path.replaceWith(t.returnStatement());
+}
+
+function ReturnStatement(path) {
+    const argument = path.node.argument;
+    if (t.isSequenceExpression(argument)) {
+        returnSequence(path);
+    } else if (t.isConditionalExpression(argument)) {
+        returnConditional(path);
+    } else if (t.unaryExpression(argument) && argument.operator === 'void') {
+        returnVoid(path);
+    } else if (t.isIdentifier(argument) && argument.name === 'undefined') {
+        returnUndefined(path);
+    }
+}
+
+module.exports = ReturnStatement;

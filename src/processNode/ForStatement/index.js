f9a16a3747fed06268e482b8ab143ca3df727894
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-03-02 02:30:55 +0100

    temporary replacement for ForStatement

--- src/processNode/ForStatement/index.js
+++ src/processNode/ForStatement/index.js
@@ -1,68 +1,21 @@
 const isExpandable = require('../../utils').isExpandable;
 const t = require('@babel/types');
 
-const getIdentifiers = require('./getIdentifiers');
-
-function extractInit(path) {
-    const init = path.node.init;
-    if (init === null || t.isVariableDeclaration(init)) {
-        return init;
-    } else if (t.isExpression(init)) {
-        return t.expressionStatement(init);
-    } else {
-        throw init;
-    }
-}
-
-function emptyTestAndUpdate(path) {
-    const prevNode = extractInit(path);
-    const forNode = t.forStatement(null, null, null, path.node.body);
-
-    const newNodes = prevNode ? [prevNode, forNode] : [forNode]
-    path.replaceWithMultiple(newNodes);
-}
-
-function emptyUpdate(path) {
-    const prevNode = extractInit(path);
-    const whileNode = t.whileStatement(path.node.test, path.node.body);
-
-    const newNodes = prevNode ? [prevNode, whileNode] : [whileNode];
-    path.replaceWithMultiple(newNodes);
-}
-
-// TODO: find some name
-function find_some_name(path) {
-    const { init, test, update, body } = path.node;
-    const [extractedStatement, remainingInit] = extractInit__(init, identifiers); // TODO: extractInit__ ??
-    const newForStatement = t.forStatement(remainingInit, test, update, body);
-    if (extractedStatement) {
-        path.replaceWithMultiple(extractedStatement, newForStatement);
-    }
-};
-
-/**
- * Heuristically separates the longest prefix of variable declarations or 
- * expressions from the `ForStatement`'s init.
- * It's achieved by listing the variables present in the `test` and `update` 
- * parts.
- * 
- * @param {*} `ForStatements`'s path 
- * @returns 
- */
 function ForStatement(path) {
-    const node = path.node;
     if (!isExpandable(path)) {
         return;
     }
-    
-    if (t.isVariableDeclaration(node.init) && node.init.kind !== 'var') {
-        return;
-    } else if (node.test === null && node.update === null) {
-        emptyTestAndUpdate(path);
-    } else if (node.update === null) {
-        emptyUpdate(path);
-    } else {
-
+    const { init, test, update, body } = path.node;
+    if (t.isVariableDeclaration(init) && init.kind === 'var') {
+        path.replaceWithMultiple([
+            t.variableDeclaration('var', init.declarations),
+            t.forStatement(null, test, update, body)
+        ]);
+    } else if (t.isExpression(init)) {
+        path.replaceWithMultiple([
+            t.expressionStatement(init),
+            t.forStatement(null, test, update, body)
+        ]);
     }
 }
 

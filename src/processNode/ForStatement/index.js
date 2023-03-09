ad9ecbe98eee75a86f70998c7f7b65b560eb3ca3
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-03-09 11:31:54 +0100

    feat: a lot

--- src/processNode/ForStatement/index.js
+++ src/processNode/ForStatement/index.js
@@ -1,22 +1,99 @@
-const isExpandable = require('../../utils').isExpandable;
 const t = require('@babel/types');
 
+const { extendSet, negate } = require('../../utils');
+
+function getIdentifiers(update) {
+    let identifiers = new Set();
+    if (t.isUpdateExpression(update) && t.isIdentifier(update.argument)) {
+        identifiers.add(update.argument.name);
+    }
+    if (t.isAssignmentExpression(update) && t.isIdentifier(update.left)) {
+        identifiers.add(update.left.name);
+    }
+    if (t.isSequenceExpression(update)) {
+        for (const expression of update.expressions)
+            extendSet(identifiers, getIdentifiers(expression));
+    }
+    return identifiers;
+}
+
+function extractInit(path, identifiers = new Set()) {
+    // TODO: implement use of identifiers
+    const { init, test, update, body } = path.node;
+    
+    if (!init)
+        return;
+    
+    if (t.isVariableDeclaration(init) && init.kind !== 'var')
+        return;
+    
+    const prevNode = t.isExpression(init) ? t.expressionStatement(init) : init;
+    const forNode = t.forStatement(null, test, update, body);
+    path.replaceWithMultiple([prevNode, forNode]);
+}
+
+function sequenceTest(path) {
+    const { init, test, update, body } = path.node;
+    
+    const ifBreak = t.ifStatement(
+        negate(test),
+        t.blockStatement([t.breakStatement()])
+    );
+    const newBody = t.blockStatement([
+        ifBreak,
+        ...body.body
+    ]);
+
+    path.replaceWith(t.forStatement(init, null, update, newBody));
+}
+
+function logicalANDTest(path) {
+    // TODO: if test is a && b && c, this will lead to two `ifBreaks`...
+    const { init, test, update, body } = path.node;
+    const { left, right } = test;
+    
+    const newTest = left;
+    const ifBreak = t.ifStatement(
+        negate(right),
+        t.blockStatement([t.breakStatement()])
+    );
+    const newBody = t.blockStatement([
+        ifBreak,
+        ...body.body
+    ]);
+
+    path.replaceWith(t.forStatement(init, newTest, update, newBody));
+}
+
+function emptyUpdate(path) {
+    const { test, body } = path.node;
+    path.replaceWith(t.whileStatement(test, body));
+}
+
 function ForStatement(path) {
-    if (!isExpandable(path)) {
+    const { init, test, update } = path.node;
+
+    if (t.isSequenceExpression(test)) {
+        sequenceTest(path);
         return;
     }
-    const { init, test, update, body } = path.node;
-    if (t.isVariableDeclaration(init) && init.kind === 'var') {
-        path.replaceWithMultiple([
-            t.variableDeclaration('var', init.declarations),
-            t.forStatement(null, test, update, body)
-        ]);
-    } else if (t.isExpression(init)) {
-        path.replaceWithMultiple([
-            t.expressionStatement(init),
-            t.forStatement(null, test, update, body)
-        ]);
+
+    if (t.isLogicalExpression(test) && test.operator === '&&') {
+        logicalANDTest(path);
+        return;
     }
+
+    if (init && !update) {
+        extractInit(path);
+        return;
+    }
+
+    if (!init && test && !update) {
+        emptyUpdate(path);
+        return;
+    }
+
+    extractInit(path);
 }
 
 module.exports = ForStatement;
--- src/processNode/IfStatement.js
+++ src/processNode/IfStatement.js
@@ -1,23 +1,263 @@
 const t = require('@babel/types');
-const isExpandable = require('../utils').isExpandable;
+const { isExpandable, willReturn, isInLambda, getTail, negate } = require('../utils');
 
-function IfStatement(path) {
-    const { test, consequent, alternate } = path.node;
-    if (!t.isSequenceExpression(test)) {
-        return;
-    }
+function sequenceExpressionTest(path) {
     if (!isExpandable(path)) {
         const warnMsg = `The following SwitchStatement is not expandable:\n${path.toString()}`;
         console.warn(warnMsg);
         return;
     }
-
+    
+    const { test, consequent, alternate } = path.node;
     const expressions = [...test.expressions];
     const newTest = expressions.pop();
     path.replaceWithMultiple([
         ...expressions.map(expr => t.expressionStatement(expr)),
         t.ifStatement(newTest, consequent, alternate)
     ]);
+}
+
+function ANDSequence1(path) {
+    // if (c && seq)
+    //     cons;
+    // ===>
+    // if (c)
+    //     if (seq)
+    //         cons;
+    const { test, consequent } = path.node;
+    const { left, right } = test;
+
+    const innerIf = t.ifStatement(right, consequent);
+    const outerIf = t.ifStatement(left, t.blockStatement([innerIf]));
+    path.replaceWith(outerIf);
+}
+
+function ANDSequence2(path) {
+    // if (c && seq)
+    //     returning_cons;
+    // else
+    //     alt;
+    // ===>
+    // if (c && seq)
+    //     returning_cons;
+    // alt;
+    if (!isExpandable(path)) {
+        throw new Error("wtf"); // TODO: improve errors
+    }
+    
+    const { test, consequent, alternate } = path.node;    
+    if (!t.isBlockStatement(alternate)) {
+        throw new Error("wtf"); // TODO: improve errors
+    }
+    path.replaceWithMultiple([
+        t.ifStatement(test, consequent),
+        ...alternate.body // alternate should be a block statement
+    ]);
+}
+
+function ORSequence1(path) {
+    // if (c || seq)
+    //     cons;
+    // else
+    //     returning_alt;
+    // ===>
+    // if (!c && !seq)
+    //     returning_alt;
+    // else
+    //     cons;
+    const { test, consequent, alternate } = path.node;
+    path.replaceWith(negate(test), alternate, consequent);
+}
+
+function ORSequence2(path) {
+    // function() {
+    //     ...
+    //     if (c || seq)
+    //         cons;
+    // }
+    // ===>
+    // function() {
+    //     ...
+    //     if (!c && !seq)
+    //         return;
+    //     cons;
+    // }
+
+    const { test, consequent } = path.node;
+    
+    if (!t.isBlockStatement(consequent)) {
+        throw new Error("wtf"); // TODO: improve errors
+    }
+    
+    const ifReturn = t.ifStatement(
+        negate(test),
+        t.blockStatement([
+            t.returnStatement()
+        ])
+    );
+    path.replaceWithMultiple([
+        ifReturn,
+        ...consequent.body
+    ]);
+}
+
+function ORSequence3(path) {
+    // function() {
+    //     ...
+    //     if (c || seq)
+    //         returning_cons;
+    //     tail;
+    // }
+    // ===>
+    // function() {
+    //     ...
+    //     if (!c && !seq) {
+    //         tail;
+    //         [return;]
+    //     }
+    //     returning_cons;
+    // }
+
+
+    const { test, consequent } = path.node;
+
+    if (!t.isBlockStatement(consequent)) {
+        throw new Error("wtf"); // TODO: improve error
+    }
+    if (!t.isBlockStatement(path.parent)) {
+        throw new Error("wtf"); // TODO: improve error
+    }
+
+    const thisIndex = path.parent.body.indexOf(path.node);
+
+    const tailStatements = path.parent.body.slice(thisIndex+1);    
+    if (!tailStatements.some(willReturn)) {
+        tailStatements.push(t.returnStatement());
+    }
+    
+    // remove tail
+    const tailPaths = path.parentPath.get('body').slice(thisIndex+1);
+    tailPaths.forEach(path => path.remove());
+    
+    path.replaceWithMultiple([
+        t.ifStatement(negate(test), t.blockStatement(tailStatements)),
+        ...consequent.body
+    ]);
+}
+
+function IfStatement(path) {
+    const { test, consequent, alternate } = path.node;
+    // if (seq, c)
+    //     cons;
+    // ===>
+    // seq;
+    // if (c)
+    //     cons;
+    if (t.isSequenceExpression(test)) {
+        sequenceExpressionTest(path);
+        return;
+    }
+    
+    // if (c && seq)
+    //     cons;
+    // ===>
+    // if (c)
+    //     if (seq)
+    //         cons;
+    if (
+        t.isLogicalExpression(test) &&
+        test.operator === '&&' &&
+        t.isSequenceExpression(test.right) &&
+        !alternate
+    ) {
+        ANDSequence1(path);
+        return;
+    }
+    
+    // if (c && seq)
+    //     returning_cons;
+    // else
+    //     alt;
+    // ===>
+    // if (c)
+    //     if (seq)
+    //         returning_cons;
+    // alt;
+    if (
+        t.isLogicalExpression(test) &&
+        test.operator === '&&' &&
+        t.isSequenceExpression(test.right) &&
+        alternate &&
+        willReturn(consequent)
+    ) {
+        ANDSequence2(path);
+        return;
+    }
+    
+    // if (c || seq)
+    //     cons;
+    // else
+    //     returning_alt;
+    // ===>
+    // if (!c && !seq)
+    //     returning_alt;
+    // else
+    //      cons;
+    if (
+        t.isLogicalExpression(test) &&
+        test.operator === '||' &&
+        t.isSequenceExpression(test.right) &&
+        willReturn(alternate)
+    ) {
+        ORSequence1(path);
+        return;
+    }
+
+    if (
+        isInLambda(path) &&
+        t.isLogicalExpression(test) &&
+        test.operator === '||' &&
+        t.isSequenceExpression(test.right)
+    ) {
+        const tail = getTail(path);
+        
+        // function() {
+        //     ...
+        //     if (c || seq)
+        //         cons;
+        // }
+        // ===>
+        // function() {
+        //     ...
+        //     if (!c && !seq)
+        //         return;
+        //     cons;
+        // }
+        if (!alternate && tail.length === 0) {
+            ORSequence2(path);
+            return;
+        }
+        
+        // function() {
+        //     ...
+        //     if (c || seq)
+        //         returning_cons;
+        //     tail;
+        // }
+        // ===>
+        // function() {
+        //     ...
+        //     if (!c && !seq) {
+        //         tail;
+        //         [return;]
+        //     }
+        //     returning_cons;
+        // }
+        if (willReturn(consequent) && !alternate) {
+            ORSequence3(path);
+            return;
+        }
+    }
 }
 
 module.exports = IfStatement;

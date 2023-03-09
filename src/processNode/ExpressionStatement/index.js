2cde7cfdaa14bebb6f627c2682309b04dc2ed1ea
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-03-09 12:08:03 +0100

    feat: assignSequence + refactor

--- src/processNode/ExpressionStatement/index.js
+++ src/processNode/ExpressionStatement/index.js
@@ -1,20 +1,97 @@
 const t = require('@babel/types');
 
-const sequenceExpression = require('./sequenceExpression');
-const conditionalExpression = require('./conditionalExpression');
-const logicalANDExpression = require('./logicalANDExpression');
-const logicalORExpression = require('./logicalORExpression');
+const { isExpandable, negate } = require('../../utils');
+
+function sequenceExpression(path) {
+    const sequenceExpression = path.node.expression;
+    const newNodes = sequenceExpression.expressions.map((expression) => {
+        return t.expressionStatement(expression)
+    });
+    
+    path.replaceWithMultiple(newNodes);
+}
+
+function conditionalExpression(path) {
+    if (!isExpandable(path)) {
+        return; // TODO: perhaps, we should throw or warn about that ?
+    }
+
+    const conditionalExpression = path.node.expression;
+    const { test, consequent, alternate } = conditionalExpression;
+    const newNode = t.ifStatement(
+        test,
+        t.blockStatement([t.expressionStatement(consequent)]),
+        t.blockStatement([t.expressionStatement(alternate)])
+    );
+
+    path.replaceWith(newNode);
+}
+
+function logicalANDExpression(path) {
+    const logicalExpression = path.node.expression;
+    const { left, right } = logicalExpression;
+    const newNode = t.ifStatement(
+        left,
+        t.blockStatement([t.expressionStatement(right)])
+    );
+    
+    path.replaceWith(newNode);
+}
+
+function logicalORExpression(path) {
+    const logicalExpression = path.node.expression;
+    const { left, right } = logicalExpression;
+    const newNode = t.ifStatement(
+        negate(left),
+        t.blockStatement([t.expressionStatement(right)])
+    );
+    
+    path.replaceWith(newNode);
+}
+
+function assignSequence(path) {
+    if (!isExpandable(path)) {
+        return; // TODO: perhaps, we should throw or warn about that ?
+    }
+
+    const { operator, left, right } = path.node.expression;
+    const expressions = [...right.expressions];
+    const newRight = expressions.pop();
+    path.replaceWithMultiple([
+        t.expressionStatement(t.sequenceExpression(expressions)),
+        t.expressionStatement(t.assignmentExpression(operator, left, newRight))
+    ]);
+}
+
+function assignConditional(path) {
+
+}
 
 function ExpressionStatement(path) {
     const expression = path.node.expression;
     if (t.isSequenceExpression(expression)) {
         sequenceExpression(path);
-    } else if (t.isConditionalExpression(expression)) {
+        return;
+    }
+    if (t.isConditionalExpression(expression)) {
         conditionalExpression(path);
-    } else if (t.isLogicalExpression(expression) && (expression.operator === '&&')) {
+        return;
+    }
+    if (t.isLogicalExpression(expression) && (expression.operator === '&&')) {
         logicalANDExpression(path);
-    } else if (t.isLogicalExpression(expression) && (expression.operator === '||')) {
+        return;
+    }
+    if (t.isLogicalExpression(expression) && (expression.operator === '||')) {
         logicalORExpression(path);
+        return;
+    }
+    if (t.isAssignmentExpression(expression) && t.isSequenceExpression(expression.right)) {
+        assignSequence(path);
+        return;
+    }
+    if (t.isAssignmentExpression(expression) && t.isConditionalExpression(expression.right)) {
+        // assignConditional(path);
+        // return;
     }
 }
 

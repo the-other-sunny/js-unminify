ac8c42caeedbeb0e9db9dacf6abe261466cfda72
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-03-11 20:41:56 +0100

    refactor

--- src/processNode/VariableDeclaration.js
+++ src/processNode/VariableDeclaration.js
@@ -1,10 +1,11 @@
-const isExpandable = require('../utils').isExpandable;
 const t = require('@babel/types');
+
+const isExpandable = require('../utils').isExpandable;
 
 function multipleDeclarators(path) {
     const node = path.node;
-    const newNodes = node.declarations.map((variableDeclarator) => {
-        return t.variableDeclaration(node.kind, [variableDeclarator])
+    const newNodes = node.declarations.map((varDeclarator) => {
+        return t.variableDeclaration(node.kind, [varDeclarator])
     });
 
     path.replaceWithMultiple(newNodes);
@@ -13,28 +14,23 @@ function multipleDeclarators(path) {
 function singleDeclaratorWithSequenceExpression(path) {
     const node = path.node;
     const kind = node.kind;
-    const variableDeclarator = node.declarations[0];
-    const id = variableDeclarator.id;
-    const sequenceExpression = variableDeclarator.init;
-    const expressionsToRemove = sequenceExpression.expressions.slice(0, -1);
-    const lastExpression = sequenceExpression.expressions.at(-1);
-    
-    const newNodes = expressionsToRemove.map((expression) => {
-        return t.expressionStatement(expression);
-    });
-    newNodes.push(
-        t.variableDeclaration(kind, [t.variableDeclarator(id, lastExpression)])
-    );
+    const { id, init } = node.declarations[0];
+    const expressions = [...init.expressions];
+    const newInit = expressions.pop();
 
-    path.replaceWithMultiple(newNodes);
+    path.replaceWithMultiple([
+        t.sequenceExpression(expressions),
+        t.variableDeclaration(kind, [t.variableDeclarator(id, newInit)])
+    ]);
 }
 
 /**
  * If the `VariableDeclaration` declares multiple variables it is turned into
  * multiple `VariableDeclaration`s each declaring a single variable.
- * If the `VariableDeclaration` declares a single variable and assigns a `SequenceExpression`
- * to it, the `SequenceExpression` is placed before the `VariableDeclaration`, except for it's
- * last `Expressions` which remains assigned to the variable.
+ * If the `VariableDeclaration` declares a single variable assigning a
+ * `SequenceExpression` to it, the `SequenceExpression` is placed before the
+ * `VariableDeclaration`, except for its last element which remains assigned
+ * to the variable.
  * 
  * @param {Path} path 
  */
@@ -46,11 +42,13 @@ function VariableDeclaration(path) {
     const node = path.node;
     if (node.declarations.length > 1) {
         multipleDeclarators(path);
-    } else if (node.declarations.length === 1) {
-        const declarator = node.declarations[0];
-        if (t.isSequenceExpression(declarator.init)) {
-            singleDeclaratorWithSequenceExpression(path);
-        }
+        return;
+    }
+    if (node.declarations.length === 1 &&
+        t.isSequenceExpression(node.declarations[0].init)
+    ) {
+        singleDeclaratorWithSequenceExpression(path);
+        return;        
     }
 }
 

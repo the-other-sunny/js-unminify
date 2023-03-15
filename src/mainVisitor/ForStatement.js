4d74a3842c26adca5b9181d2b1058753821fdbd5
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-03-15 14:57:59 +0100

    feat + test: extractInit

--- src/mainVisitor/ForStatement.js
+++ src/mainVisitor/ForStatement.js
@@ -2,34 +2,130 @@ const t = require('@babel/types');
 
 const { extendSet, negate } = require('../utils');
 
-function getIdentifiers(update) {
-    let identifiers = new Set();
-    if (t.isUpdateExpression(update) && t.isIdentifier(update.argument)) {
-        identifiers.add(update.argument.name);
-    }
-    if (t.isAssignmentExpression(update) && t.isIdentifier(update.left)) {
-        identifiers.add(update.left.name);
-    }
-    if (t.isSequenceExpression(update)) {
-        for (const expression of update.expressions)
-            extendSet(identifiers, getIdentifiers(expression));
-    }
-    return identifiers;
-}
-
-function extractInit(path, identifiers = new Set()) {
+function extractInit(path) {
     // TODO: implement use of identifiers
     const { init, test, update, body } = path.node;
-    
-    if (!init)
-        return;
-    
-    if (t.isVariableDeclaration(init) && init.kind !== 'var')
-        return;
-    
-    const prevNode = t.isExpression(init) ? t.expressionStatement(init) : init;
-    const forNode = t.forStatement(null, test, update, body);
-    path.replaceWithMultiple([prevNode, forNode]);
+    let changed = false;    
+
+    if (!init) {
+        return changed;
+    }
+
+    if (t.isVariableDeclaration(init)) {
+        changed = extractVariableDeclaration(path);
+    }
+
+    if (t.isExpression(init)) {
+        changed = extractExpression(path);
+    }
+
+    return changed;
+
+    function getIdentifiers(update) {
+        const identifiers = new Set();
+        if (t.isUpdateExpression(update) && t.isIdentifier(update.argument)) {
+            identifiers.add(update.argument.name);
+        }
+        if (t.isAssignmentExpression(update) && t.isIdentifier(update.left)) {
+            identifiers.add(update.left.name);
+        }
+        if (t.isSequenceExpression(update)) {
+            update.expressions.forEach(expression => {
+                extendSet(identifiers, getIdentifiers(expression));
+            });
+        }
+        return identifiers;
+    }
+
+    function extractVariableDeclaration(path) {
+        const { init, test, update, body } = path.node;
+        const { kind, declarations } = init;
+        let changed = false;
+        
+        if (init.kind !== 'var') {
+            return changed;
+        }
+
+        const maintainedIdentifiers = getIdentifiers(update); 
+        
+        let index = declarations.findIndex(decl => {
+            const id = decl.id;
+            return t.isIdentifier(id) && maintainedIdentifiers.has(id.name);
+        });
+
+        if (index == -1) {
+            index = declarations.length;
+        }
+
+        if (index == 0) {
+            return changed;
+        }
+
+        const extractedDecls = declarations.slice(0, index);
+        const maintainedDecls = declarations.slice(index);
+        const newInit = maintainedDecls.length ?
+                        t.variableDeclaration(kind, maintainedDecls):
+                        null;
+
+        path.replaceWithMultiple([
+            t.variableDeclaration(kind, extractedDecls),    
+            t.forStatement(
+                newInit,
+                test,
+                update,
+                body
+            )
+        ]);
+        
+        changed = true;
+
+        return changed;
+    }
+
+    function extractExpression(path) {
+        const { init, test, update, body } = path.node;
+
+        const expressions = t.isSequenceExpression(init) ?
+                            init.expressions:
+                            [init];
+        let changed = false;
+        const maintainedIdentifiers = getIdentifiers(update); 
+
+        let index = expressions.findIndex(expr => {
+            return t.isAssignmentExpression(expr) && 
+                   t.isIdentifier(expr.left) &&
+                   maintainedIdentifiers.has(expr.left.name); 
+        });
+
+        if (index == -1) {
+            index = expressions.length;
+        }
+
+        if (index == 0) {
+            return changed;
+        }
+
+        const extractedExpressions = expressions.slice(0, index);
+        const maintainedExpressions = expressions.slice(index);
+
+        const newInit = maintainedExpressions.length ?
+                        t.sequenceExpression(maintainedExpressions):
+                        null;
+
+        path.replaceWithMultiple([
+            t.expressionStatement(t.sequenceExpression(extractedExpressions)),
+            t.forStatement(
+                newInit,
+                test,
+                update,
+                body
+            )
+        ]);
+        
+        changed = true;
+
+        return changed;
+    }
 }
 
 function sequenceTest(path) {
@@ -72,6 +168,11 @@ function emptyUpdate(path) {
 
 function ForStatement(path) {
     const { init, test, update } = path.node;
+    const changed = extractInit(path);
+    
+    if (changed) {
+        return;
+    }
 
     if (t.isSequenceExpression(test)) {
         sequenceTest(path);
@@ -83,17 +184,10 @@ function ForStatement(path) {
         return;
     }
 
-    if (init && !update) {
-        extractInit(path);
-        return;
-    }
-
     if (!init && test && !update) {
         emptyUpdate(path);
         return;
     }
-
-    extractInit(path);
 }
 
 module.exports = ForStatement;
--- test/io/mainVisitor/ForStatement.io.js
+++ test/io/mainVisitor/ForStatement.io.js
@@ -1,10 +1,110 @@
 test1: {
-    input: {
-        for (; a, b, c;) {
-            foo();
-        }
+  input: {
+    for (var a, b, c, i = 0, iMax = 10; i <= iMax; ++i) {
+      foo();
     }
-    expected_output: {
-        
+  }
+  expected_output: {
+    var a;
+    var b;
+    var c;
+    for (var i = 0, iMax = 10; i <= iMax; ++i) {
+      foo();
     }
+  }
+}
+test2: {
+  input: {
+    for (var a, b, c; i <= iMax; ++i) {
+      foo();
+    }
+  }
+  expected_output: {
+    var a;
+    var b;
+    var c;
+    for (; i <= iMax; ++i) {
+      foo();
+    }
+  }
+}
+test3: {
+  input: {
+    for (var i = 0; i <= iMax; ++i) {
+      foo();
+    }
+  }
+  expected_output: {
+    for (var i = 0; i <= iMax; ++i) {
+      foo();
+    }
+  }
+}
+test4: {
+  input: {
+    for (var a; i <= iMax; ++i) {
+      foo();
+    }
+  }
+  expected_output: {
+    var a;
+    for (; i <= iMax; ++i) {
+      foo();
+    }
+  }
+}
+test5: {
+  input: {
+    for (a, b, c, i = 0, iMax = 10; i <= iMax; ++i) {
+      foo();
+    }
+  }
+  expected_output: {
+    a;
+    b;
+    c;
+    for (i = 0, iMax = 10; i <= iMax; ++i) {
+      foo();
+    }
+  }
+}
+test6: {
+  input: {
+    for (a, b, c; i <= iMax; ++i) {
+      foo();
+    }
+  }
+  expected_output: {
+    a;
+    b;
+    c;
+    for (; i <= iMax; ++i) {
+      foo();
+    }
+  }
+}
+test7: {
+  input: {
+    for (i = 0; i <= iMax; ++i) {
+      foo();
+    }
+  }
+  expected_output: {
+    for (i = 0; i <= iMax; ++i) {
+      foo();
+    }
+  }
+}
+test8: {
+  input: {
+    for (a; i <= iMax; ++i) {
+      foo();
+    }
+  }
+  expected_output: {
+    a;
+    for (; i <= iMax; ++i) {
+      foo();
+    }
+  }
 }
